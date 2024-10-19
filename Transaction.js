6b9b9d088e2790de902a42bfc6e51282607d9a71
Yash Repal <128345420+YashRepal@users.noreply.github.com>
2024-10-19 10:58:28 +0530

    Add files via upload

---
+++ Transaction.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const transactionSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    price: Number,
+    dateOfSale: Date,
+    category: String,
+    sold: Boolean
+});
+
+module.exports = mongoose.model('Transaction', transactionSchema);
+http://localhost:5000/api/transactions
